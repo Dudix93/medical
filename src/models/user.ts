2f030add962c04b6fdc9e74e0b5970407d5ef6bd
Michal Dodot <michal.dodot.93@gmail.com>
2017-10-26 18:20:13 +0200

    changing user data

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,7 +1,9 @@
-export class NewUser {
+export class User {
     constructor(
+        public id:any,
         public login:any,
         public name:any,
+        public email:any,
         public password:any,
         public projects:any,
         public tasks:any){}
--- src/pages/edit-profile/edit-profile.ts
+++ src/pages/edit-profile/edit-profile.ts
@@ -26,18 +26,35 @@ export class EditProfilePage {
           this.user.push(data);
         });
       });
+      console.log(this.user);
   }
 
 update(){
-  if(this.old_password == undefined){
+  if(this.old_password == undefined && (this.new_password == undefined || this.new_password2 == undefined)){
     this.restapiService.deleteUser(this.user[0].id);
     this.restapiService.saveUser(new User(
+      this.user[0].id,
       this.user[0].login,
       this.user[0].name,
+      this.user[0].email,
       this.user[0].password,
       this.user[0].projects,
       this.user[0].tasks
     ));
+  }
+  else{
+    if((this.new_password == this.new_password2) && this.old_password == this.user[0].password && this.old_password != undefined && this.new_password != null && this.new_password2 != null && this.new_password != undefined && this.new_password2 != undefined){
+      this.restapiService.deleteUser(this.user[0].id);
+      this.restapiService.saveUser(new User(
+        this.user[0].id,
+        this.user[0].login,
+        this.user[0].name,
+        this.user[0].email,
+        this.new_password,
+        this.user[0].projects,
+        this.user[0].tasks
+      ));
+    }
   }
 }
 }
