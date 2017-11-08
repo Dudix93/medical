7ac5fd402124a32b223b8842c12ac022b8b60a68
Dudix93 <dodo45693@gmail.com>
2017-11-08 23:20:24 +0100

    fixed task edit(description & check if time is number & integer)

--- src/pages/edit-task/edit-task.ts
+++ src/pages/edit-task/edit-task.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Storage } from '@ionic/storage';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 import { RestapiServiceProvider } from '../../providers/restapi-service/restapi-service';
 
 @Component({
@@ -38,7 +38,11 @@ export class EditTaskPage {
     finish_date:'',
     finish_hour:''}
 
-  constructor(public navCtrl: NavController, public navParams:NavParams, private restapiService: RestapiServiceProvider, private storage: Storage) {
+  constructor(public navCtrl: NavController, 
+              public navParams:NavParams, 
+              private restapiService: RestapiServiceProvider, 
+              private storage: Storage,
+              private alertCtrl: AlertController) {
     this.params.task_title = this.navParams.get('task_title');
     this.params.task_id = this.navParams.get('task_id');
     this.getUserTask(this.params.task_id);
@@ -72,43 +76,69 @@ export class EditTaskPage {
 }
 
 updateTask(){
-  //console.log("date "+this.task.update_date+" hour "+this.task.update_hour+" time "+this.task.update_time+" latest_dayTask "+this.task.latest_dayTask);
-  if(this.updateTime != undefined && this.updateTime != ''){
-    this.task.time_spent += Number(this.updateTime);
-    this.task.update_time = Number(this.updateTime);
-
-    this.dayTask.date = new Date().toLocaleDateString();
-    this.dayTask.hour = this.getHour();
-    this.dayTask.time_spent = this.task.time_spent;
-    this.dayTask.task_id = Number(this.task.task_id);
-    this.dayTask.user_id = Number(this.task.user_id);
-    console.log(this.task);
-    console.log("update time: "+this.task.update_date);
-    if(this.task.update_date == new Date().toLocaleDateString() || this.task.update_date == null){
-      console.log("update: "+this.task.latest_dayTask);
-      this.restapiService.updateDayTask(this.task.latest_dayTask, this.dayTask);
+  if(this.updateTime != undefined && this.updateTime != '' && isNaN(Number(this.updateTime)) == true){
+    this.showalert("Wpisz liczbę!");
+  }
+  else if(this.updateTime != undefined && this.updateTime != '' && Number(this.updateTime) !== parseInt(this.updateTime, 10)){
+    this.showalert("Wpisz pełną godzine!");
+  }
+  else{
+    if(this.updateTime != undefined && this.updateTime != ''){
+      this.task.time_spent += Number(this.updateTime);
+      this.task.update_time = Number(this.updateTime);
+  
+      this.dayTask.date = new Date().toLocaleDateString();
+      this.dayTask.hour = this.getHour();
+      this.dayTask.time_spent = this.task.time_spent;
+      this.dayTask.task_id = Number(this.task.task_id);
+      this.dayTask.user_id = Number(this.task.user_id);
+      console.log(this.task);
+      console.log("update time: "+this.task.update_date);
+      if(this.task.update_date == new Date().toLocaleDateString() || this.task.update_date == null){
+        console.log("update: "+this.task.latest_dayTask);
+        this.restapiService.updateDayTask(this.task.latest_dayTask, this.dayTask);
+      }
+      else{
+        this.restapiService.saveDayTask(this.dayTask);
+      }
+  
+      this.task.update_date = new Date().toLocaleDateString();
+      this.task.update_hour = this.getHour();
+      //console.log("task id: "+this.task.task_id);
+      this.restapiService.getLatestDayTask(Number(this.task.task_id)).then(data => {
+        this.dayTasks = new Array<number>();
+        this.dayTasksObjects = data;
+        //console.log(data);
+        for(let dayTask of this.dayTasksObjects){
+          this.task.latest_dayTask = dayTask.id;
+          //console.log(dayTask.id);
+          break;
+        }
+        //console.log("date "+this.task.update_date+" hour "+this.task.update_hour+" time "+this.task.update_time+" latest_dayTask "+this.task.latest_dayTask);
+        this.restapiService.updateUserTask(this.task.id,this.task);
+        this.getUserTask(this.params.task_id);
+      });
     }
     else{
-      this.restapiService.saveDayTask(this.dayTask);
-    }
-
-    this.task.update_date = new Date().toLocaleDateString();
-    this.task.update_hour = this.getHour();
-    //console.log("task id: "+this.task.task_id);
-    this.restapiService.getLatestDayTask(Number(this.task.task_id)).then(data => {
-      this.dayTasks = new Array<number>();
-      this.dayTasksObjects = data;
-      //console.log(data);
-      for(let dayTask of this.dayTasksObjects){
-        this.task.latest_dayTask = dayTask.id;
-        //console.log(dayTask.id);
-        break;
-      }
-      //console.log("date "+this.task.update_date+" hour "+this.task.update_hour+" time "+this.task.update_time+" latest_dayTask "+this.task.latest_dayTask);
       this.restapiService.updateUserTask(this.task.id,this.task);
       this.getUserTask(this.params.task_id);
-    });
+    } 
   }
+}
+
+showalert(info:string) {
+  const alert = this.alertCtrl.create({
+    title: info,
+    buttons: [
+      {
+        text: 'Ok',
+        handler: () => {
+          //this.startTask(data);
+        }
+      }
+    ]
+  });
+  alert.present();
 }
 
 }
